311cf360bd5a3e345bc84c01c646a2eab48e1feb
RK-Saleh <168818921+RK-Saleh@users.noreply.github.com>
2025-06-16 16:46:50 +0200

    Update Voltage_divider.ino

--- Code Testing/Voltage_divider/Voltage_divider.ino
+++ Code Testing/Voltage_divider/Voltage_divider.ino
@@ -10,74 +10,122 @@
 #define trigPin 12
 #define echoPin 13
 
-int forwardSpeed = 100;      // Increased speed for better response
-int turnSpeedLeft = 60;      // For turning left: reduce left motor speed
-int turnSpeedRight = 60;     // For turning right: reduce right motor speed
-int approachThreshold = 20;  // obstacle distance in cm
+float distanceFromObject = 5000;
+
+int baseSpeed = 100;
+int turnSpeed = 70;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(R_S, INPUT);
+  
   pinMode(L_S, INPUT);
+  pinMode(R_S, INPUT);
+
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+}
+
+void activateTriggerPin() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+}
+
+float measureDistance() {
+  activateTriggerPin();
+  long travelTime = pulseIn(echoPin, HIGH, 30000);
+  return travelTime * 0.034 / 2;  // Distance in cm
+}
+
+void motorAForward(int speed) {
+  analogWrite(enA, speed);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+}
+
+void motorBForward(int speed) {
+  analogWrite(enB, speed);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void motorABackward(int speed) {
+  analogWrite(enA, speed);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+}
+
+void motorBBackward(int speed) {
+  analogWrite(enB, speed);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void forward(int speed) {
+  motorAForward(speed);
+  motorBForward(speed);
+}
+
+void backward(int speed) {
+  motorABackward(speed);
+  motorBBackward(speed);
+}
+
+void stopMotors() {
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
+}
+
+void turn180() {
+  motorABackward(baseSpeed);
+  motorBForward(baseSpeed);
+  delay(1000);
+  stopMotors();
+}
+
+void decelerate() {
+  for (int i = baseSpeed; i > 0; i -= 30) {
+    forward(i);
+    delay(100);
+  }
+}
+
+void avoidObject() {
+  distanceFromObject = measureDistance();
+  if (distanceFromObject <= 20.0) {
+    Serial.println("Obstacle Found");
+    stopMotors();
+    delay(10);
+  }
 }
 
 void loop() {
   int leftIR = digitalRead(L_S);
   int rightIR = digitalRead(R_S);
-  int distance = getDistance();
 
-  Serial.print("L_IR="); Serial.print(leftIR);
-  Serial.print(" | R_IR="); Serial.print(rightIR);
-  Serial.print(" | Distance="); Serial.println(distance);
+  avoidObject();
 
-  // Obstacle detected
-  if (distance > 0 && distance <= approachThreshold) {
-    Stop();
-    Serial.println("Obstacle ahead! Stopping.");
-    delay(500);
-    return;
-  }
-
-  // Line following logic
-  if (leftIR == 0 && rightIR == 0) {
-    moveForward(forwardSpeed, forwardSpeed);  // Go straight
-  } else if (leftIR == 1 && rightIR == 0) {
-    moveForward(turnSpeedLeft, forwardSpeed);  // Turn left gently
-  } else if (leftIR == 0 && rightIR == 1) {
-    moveForward(forwardSpeed, turnSpeedRight); // Turn right gently
+  if (leftIR == HIGH && rightIR == HIGH) {
+    forward(baseSpeed);  // on black line
+  } else if (leftIR == LOW && rightIR == HIGH) {
+    motorABackward(150);
+    motorBForward(baseSpeed + turnSpeed);
+  } else if (leftIR == HIGH && rightIR == LOW) {
+    motorAForward(baseSpeed + turnSpeed);
+    motorBBackward(150);
   } else {
-    Stop();  // Both black or undefined
+    backward(baseSpeed);
   }
-}
-
-// Smooth motor control
-void moveForward(int speedA, int speedB) {
-  analogWrite(enA, speedA);
-  analogWrite(enB, speedB);
-  digitalWrite(in1, HIGH); digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW); digitalWrite(in4, HIGH);
-}
-
-void Stop() {
-  analogWrite(enA, 0);
-  analogWrite(enB, 0);
-  digitalWrite(in1, LOW); digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW); digitalWrite(in4, LOW);
-}
-
-// Ultrasonic distance in cm
-int getDistance() {
-  digitalWrite(trigPin, LOW); delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH, 20000); // timeout 20ms
-  return duration * 0.034 / 2;
 }
